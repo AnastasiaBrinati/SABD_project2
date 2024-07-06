ce7527e98b96ca5d46045650ff1fe135e99f0bf7
AnastasiaBrinati <birillo347@gmail.com>
2024-07-06 20:29:14 +0200

    added another taskmanager, and the result consumer

--- consume.sh
+++ consume.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-docker stop taskmanager
-docker rm taskmanager
+docker stop taskmanager1
+docker rm taskmanager1
+docker stop taskmanager2
+docker rm taskmanager2
 docker stop jobmanager
 docker rm jobmanager
 
@@ -11,7 +13,6 @@ docker build -t flink-taskmanager-image -f ./consumer/taskmanager.dockerfile ./c
 
 # Run the JobManager container
 docker run -d --name=jobmanager --network project2-network -p 8081:8081 -p 6123:6123 \
-  --volume ./Results:/Results \
   --volume ./consumer/src:/src \
   --workdir /opt/flink/src/ \
   -e "FLINK_PROPERTIES=jobmanager.rpc.address: jobmanager" \
@@ -21,8 +22,13 @@ docker run -d --name=jobmanager --network project2-network -p 8081:8081 -p 6123:
 # Pause to allow JobManager to start
 echo "Waiting for JobManager to start..."
 
-# Run the TaskManager container
-docker run -d --name=taskmanager --network project2-network --volume ./Results:/Results \
+# Run the TaskManager containers
+docker run -d --name=taskmanager1 --network project2-network \
+  -e "FLINK_PROPERTIES=jobmanager.rpc.address: jobmanager" \
+  flink-taskmanager-image \
+  taskmanager
+
+docker run -d --name=taskmanager2 --network project2-network \
   -e "FLINK_PROPERTIES=jobmanager.rpc.address: jobmanager" \
   flink-taskmanager-image \
   taskmanager
